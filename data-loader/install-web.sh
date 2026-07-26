c9a93644fb13d44d1400d0c67dbecdf1ac549595
jteresco <terescoj@gmail.com>
2026-07-26 19:40:57 -0400

    Updates to get closer to a usable web site.

--- data-loader/install-web.sh
+++ data-loader/install-web.sh
@@ -2,15 +2,25 @@
 # install-web.sh
 set -e
 
-echo "Updating web assets from /app/repos/Web..."
+echo "Updating web assets from /app/Web..."
 
 # 1. Update /web/highways
-echo "Deploying to /web/highways..."
-cd /app/repos/Web
-/app/repos/Web/install-local.sh /web/highways
+echo "Deploying to /web/highways...!"
+cd /app/Web
+/app/Web/updatelocal.sh /web/highways
+echo "highways" > /web/highways/lib/tm.conf
+echo "TravelMapping" >> /web/highways/lib/tm.conf
+echo "travmap" >> /web/highways/lib/tm.conf
+echo "travmap_password" >> /web/highways/lib/tm.conf
+echo "localhost" >> /web/highways/lib/tm.conf
+echo "HEREID" >> /web/highways/lib/tm.conf
+echo "HEREAPIKEY" >> /web/highways/lib/tm.conf
+echo "TKKEY" >> /web/highways/lib/tm.conf
+echo "MAPBOXKEY" >> /web/highways/lib/tm.conf
+echo "JAWGTOKEN" >> /web/highways/lib/tm.conf
 
 # 2. Update /web/rail
 echo "Deploying to /web/rail..."
-/app/repos/Web/install-local.sh /web/rail
+/app/Web/updatelocal.sh /web/rail
 
 echo "Web assets deployment complete!"
