32fe005a4372115b783efa584795c7d074b8adce
jteresco <terescoj@gmail.com>
2026-07-26 21:57:12 -0400

    First functional TM Highways in Docker.

--- data-loader/install-web.sh
+++ data-loader/install-web.sh
@@ -5,19 +5,27 @@ set -e
 echo "Updating web assets from /app/Web..."
 
 # 1. Update /web/highways
-echo "Deploying to /web/highways...!"
+echo "Deploying to /web/highways..."
 cd /app/Web
 /app/Web/updatelocal.sh /web/highways
+
+# create a tm.conf
 echo "highways" > /web/highways/lib/tm.conf
 echo "TravelMapping" >> /web/highways/lib/tm.conf
 echo "travmap" >> /web/highways/lib/tm.conf
 echo "travmap_password" >> /web/highways/lib/tm.conf
-echo "localhost" >> /web/highways/lib/tm.conf
+echo "db" >> /web/highways/lib/tm.conf
 echo "HEREID" >> /web/highways/lib/tm.conf
 echo "HEREAPIKEY" >> /web/highways/lib/tm.conf
 echo "TKKEY" >> /web/highways/lib/tm.conf
 echo "MAPBOXKEY" >> /web/highways/lib/tm.conf
 echo "JAWGTOKEN" >> /web/highways/lib/tm.conf
+
+# create motd
+echo "This is an instance of Travel Mapping Highways running in a Docker environment.  The production TM server is <a href=\"https://travelmapping.net\">here</a>" > /web/highways/motd
+
+echo "Installing custom Leaflet to /web/highways..."
+cp -r /app/leaflet-1.9.4 /web/highways/
 
 # 2. Update /web/rail
 echo "Deploying to /web/rail..."
